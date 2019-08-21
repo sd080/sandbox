0ab018e648357d1d254b435b2b2783c224e30b9a
sd080 <slaed.danielsramsey@my.jcu.edu.au>
2019-08-21 14:52:22 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """Slaed Daniels-Ramsey"""
+
+name = input("Please enter password")
+
+if len(name) < 10:
+    print("Your password must be 10 characters long.")
+
+else:
+    print("Password accepted")
